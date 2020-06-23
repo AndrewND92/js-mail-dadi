2abe21b0513070605de5e922e102d070415e22ce
Andrea <miranda.andrea.nd@gmail.com>
2020-06-23 16:01:32 +0200

    finito ma con il math.random sbagliato, ci sono numeri da 0 a 6

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var utente;
 var computer;
 var max = 6;
 var min = 1;
+var risultato;
 
 document.getElementById("btn").addEventListener("click",function() {
 
@@ -9,11 +10,14 @@ document.getElementById("btn").addEventListener("click",function() {
   computer = Math.floor(Math.random() * (max + min)) ;
 
   if (utente > computer) {
-    console.log(utente, "vince l'utente");
+    risultato = utente + " vince l'utente";
+    // console.log(utente, "vince l'utente");
 
   }else if (computer > utente){
-    console.log(computer, "vince il computer");
+    risultato = utente + " vince il computer";
+    // console.log(computer, "vince il computer");
 
   }else ("pareggio");
 
+ document.getElementById("result").innerHTML = risultato ;
 });
